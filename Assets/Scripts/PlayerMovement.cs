df3f060503bcd90215b22dfdbf9ded11b51ce160
Seth Farrar <seth@ai-sys.com>
2018-05-10 13:40:42 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Vector2 moveVelocity;
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		moveVelocity = input * speed;
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
